90a43dc29ee634cc09ed3547d197d0bd99e5e20b
vagosdim <cse32434@cs.uoi.gr>
2018-11-29 18:58:30 +0200

    Implement basic login

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'pc/new'
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
@@ -6,5 +7,9 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
   get '/signup',   to: 'users#new'
 
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
   resources :users
 end

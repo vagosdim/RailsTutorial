3217824c5a0643a5e404a0af23ae3e6849f6e630
vagosdim <cse32434@cs.uoi.gr>
2018-12-02 18:20:00 +0200

    Add user microposts

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   
   resources :users
+  resources :microposts,          only: [:create, :destroy]
+
 end
--- sample_app/db/seeds.rb
+++ sample_app/db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
@@ -19,4 +20,11 @@ User.create!(name:  "Example User",
                email: email,
                password:              password,
                password_confirmation: password)
+
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
